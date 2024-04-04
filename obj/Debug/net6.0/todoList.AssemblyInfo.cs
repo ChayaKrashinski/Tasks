79c76bfe72a59a4c99958a3c3c12728cd61adb69
Chaya Krashinski <a0527651529@gmail.com>
2024-04-04 13:40:16 +0300

    delete and edit tasks

--- obj/Debug/net6.0/todoList.AssemblyInfo.cs
+++ obj/Debug/net6.0/todoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5a351b505a77797b6a6379b307d143bc9b73e04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+519ea92fe49e25da85aea99afe2f704f40942fd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("todoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -49,7 +49,7 @@ const drawTable = () => {
     th5.innerHTML = 'Delete'
 
     const chn = [th1, th2, th3, th4, th5]
-    chn.forEach(ch=>tr.appendChild(ch))
+    chn.forEach(ch => tr.appendChild(ch))
     const tbody = document.createElement('tbody')
     table.appendChild(tr)
     table.appendChild(tbody)
@@ -60,64 +60,105 @@ const drawTask = (task) => {
     const tasksList = document.getElementById('tasksList');
     const tr = document.createElement('tr')
 
-    const th1 = document.createElement('th')
+    const edit = document.createElement('th')
     const editButton = document.createElement('button')
     editButton.innerHTML = 'edit'
 
-    const th2 = document.createElement('th')
-    th2.innerHTML = task.id;
+    const id = document.createElement('th')
+    id.innerHTML = task.id;
 
-    const th3 = document.createElement('th')
-    const input3 = document.createElement('input')
-    input3.type = Text;
-    input3.value = task.name
+    const name = document.createElement('th')
+    const nameInput = document.createElement('input')
+    nameInput.type = Text;
+    nameInput.value = task.name
 
-    const th4 = document.createElement('th')
-    const input4 = document.createElement('input')
-    input4.type = Text;
-    input4.value = task.isDone
+    const isDone = document.createElement('th')
+    const isDoneInput = document.createElement('input')
+    isDoneInput.type = 'checkbox';
+    isDoneInput.checked = task.isDone
 
-    const th5 = document.createElement('th')
+    const Delete = document.createElement('th')
     const deleteButton = document.createElement('button')
     deleteButton.innerHTML = 'delete'
 
-    input3.disabled = true;
-    input4.disabled = true;
+    nameInput.disabled = true;
+    isDoneInput.disabled = true;
 
-    editButton.onclick = () => {
+     editButton.onclick = () => {
         if (editButton.innerHTML == 'edit') {
             editButton.innerHTML = 'ok'
-            input3.disabled = false;
-            input4.disabled = false;
+            nameInput.disabled = false;
+            isDoneInput.disabled = false;
         }
         else {
-            editButton.innerHTML = 'edit'
-            input3.disabled = true;
-            input4.disabled = true;
+            const newTask = {
+                "id": task.id,
+                "name": nameInput.value.trim(),
+                "isDone": isDoneInput.checked
+            }
+            fetch(`${todoURL}/UpdateTask/${task.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Authorization': auth,
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newTask)
+            })
+                .then(response => {
+                    alert(response.status)
+                    editButton.innerHTML = 'edit'
+                    nameInput.disabled = true;
+                    isDoneInput.disabled = true;
+                })
+                .catch(error => {
+                    console.error('error:', error)
+                    alert("noData!!")
+                    // location.href = "login.js"
+                });
+
         }
     }
 
     deleteButton.onclick = () => {
-        tasksList.removeChild(tr)
+        fetch(`${todoURL}/DeleteTask/${task.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': auth,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(response => {
+                tasksList.removeChild(tr)
+                alert('the task deleted')
+            })
+            .catch(error => {
+                console.error('error:', error)
+                alert("error!!")
+                // location.href = "login.js"
+            });
+
     }
 
-    th1.appendChild(editButton)
-    th3.appendChild(input3)
-    th4.appendChild(input4)
-    th5.appendChild(deleteButton)
+    edit.appendChild(editButton)
+    name.appendChild(nameInput)
+    isDone.appendChild(isDoneInput)
+    Delete.appendChild(deleteButton)
 
-    tr.appendChild(th1)
-    tr.appendChild(th2)
-    tr.appendChild(th3)
-    tr.appendChild(th4)
-    tr.appendChild(th5)
+    tr.appendChild(edit)
+    tr.appendChild(id)
+    tr.appendChild(name)
+    tr.appendChild(isDone)
+    tr.appendChild(Delete)
 
     tasksList.appendChild(tr)
 }
 
 
-
-function GetMyTasks() {
+const tasksBtn = document.getElementById('tasksBtn')
+tasksBtn.onclick = () => {
+    tasksBtn.disabled = false
     fetch(`${todoURL}/tasksList`, {
         method: 'GET',
         headers: {
@@ -135,6 +176,7 @@ function GetMyTasks() {
         .catch(error => {
             console.error('error:', error)
             alert("noData!!")
+            // location.href = "login.js"
         });
 }
 
